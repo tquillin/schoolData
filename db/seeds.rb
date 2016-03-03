25376dae59962bfba96607868b96fedceb844e19
Terrell Quillin <terrellq@gmail.com>
2016-03-03 10:24:51 -0700

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,12 @@
-require 'csv'
-
-puts 'Parsing File'
-
-data = CSV.foreach('./School_Progress_Report_2010-2011.csv', headers: true).map do |row|
-  {
-    name: row['SCHOOL'],
-    admin: row['PRINCIPAL'],
-    level: row['SCHOOL LEVEL'],
-    grade: row['2010-2011 OVERALL GRADE']
-  }
-end
-
-puts 'Placing in Database'
-Score.create(data);
+Score.create!([
+  {name: "P.S. 015 Roberto Clemente", admin: "Irene Sanchez", level: nil, grade: "C"},
+  {name: "P.S. 019 Asher Levy", admin: "Jacqueline Flanagan", level: nil, grade: "B"},
+  {name: "P.S. 020 Anna Silver", admin: "James Lee", level: nil, grade: "B"},
+  {name: "P.S. 034 Franklin D. Roosevelt", admin: "Joyce Stallings Harte", level: nil, grade: "C"},
+  {name: "P.S. 063 William McKinley", admin: "Darlene Despeignes", level: nil, grade: "B"},
+  {name: "P.S. 064 Robert Simon", admin: "Marlon L. Hosang", level: nil, grade: "C"},
+  {name: "P.S. 110 Florence Nightingale", admin: "Karen Feuer", level: nil, grade: "C"},
+  {name: "P.S. 134 Henrietta Szold", admin: "Loretta Caputo", level: nil, grade: "C"},
+  {name: "P.S. 137 John L. Bernstein", admin: "Melissa Rodriguez", level: nil, grade: "F"},
+  {name: "P.S. 140 Nathan Straus", admin: "Esteban Barrientos", level: nil, grade: "D"}
+])
